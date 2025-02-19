f1b5cb324d645a634284f88de9c1b3fb1f0ba73f
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-02-20 02:57:03 +0600

    v3
    Start Templates Rendering

--- hello/home/views.py
+++ hello/home/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
-def index(request):
-    #return HttpResponse("This is my home page")
-    return render(request, 'index.html')
 
+#Home
+def index(request):
+
+    context ={
+        'variable1':"This is from variable1 context.",
+        'variable2':"This is from variable2 context.",
+        'variable3':"This is from variable3 context.",
+    }
+
+    #return HttpResponse("This is my home page")
+    return render(request, 'index.html', context)
+
+#About
 def about(request):
     return HttpResponse("This is my about page")
 
+#Services
 def services(request):
     return HttpResponse("This is my services page")
 
+#Contact
 def contact(request):
     return HttpResponse("This is my contact page")
 
