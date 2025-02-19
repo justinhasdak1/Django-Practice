a418acbd2a8386f33a39e6fee4bf9e129b40706b
Matheus Justin Hasda <matheusjustinhasda@gmail.com>
2025-02-20 02:39:35 +0600

    v2
    Connect Static & Template files

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hello.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +117,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#added manuelly
+import os
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- hello/home/views.py
+++ hello/home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is my home page")
+    #return HttpResponse("This is my home page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("This is my about page")
